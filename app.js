4a6438ff77556d96d753a05f9158c095dc693406
Aishwarya <aishwarya.verma28@gmail.com>
2022-12-04 20:25:19 +0530

    responsive nav bar

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-//queryselectors to select html class
+//===========================================queryselectors to select html class======================================
 let mobileNav = document.querySelector('.nav-list');
 let hamburger = document.querySelector('.hamburger');
 let bars = document.querySelectorAll('.hamburger span');
 let isActive = false;
-//eventlisteners
+//==========================================================functions==================================================
+function addClass(){
+    if(window.innerWidth < 860){
+        document.body.classList.add('mobile');
+    }
+    else{
+        document.body.classList.remove('mobile');
+    }
+}
+//need to execute this function when window is loaded
+ window.onload = addClass
+//==========================================================eventlisteners=================================================
+//it will trigered whenever window's size is changed
+window.addEventListener('resize', function(){
+    addClass();
+})
 //hamburger icon event
 hamburger.addEventListener('click', function() {
     mobileNav.classList.toggle('open');
-    bars[0].style.transform = 'rotate(45deg)';
-    bars[1].style.opacity = '0';
-    bars[2].style.transform = 'rotate(-45deg)';
+    if(isActive==false){
+        bars[0].style.transform = 'rotate(45deg)';
+        bars[1].style.opacity = '0';
+        bars[2].style.transform = 'rotate(-45deg)';
+        isActive = true;
+    }
+    else
+    {
+        bars[0].style.transform = 'rotate(0deg)';
+        bars[1].style.opacity = '1';
+        bars[2].style.transform = 'rotate(0deg)';
+        isActive = false
+    }
 })
